d7f8c2cde12c4dd2197c3e7a3d8f02e4f781c148
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-07-29 13:24:44 +0900

    updated main

--- ReturnValues.java
+++ ReturnValues.java
@@ -1,12 +1,11 @@
 public class ReturnValues {
   public static void main(String[] args) {
     double area1 = calculateArea(2.3, 3.6);
-    double area2 = calculateArea(1.6, 2.4);
-    double area3 = calculateArea(2.6, 4.2);
+    printArea(2.3, 3.6, area1);
 
     String englishExplanation = explainArea("English");
-    String frenchExplanation = explainArea("French");
-    String spanishExplanation = explainArea("Spanish");
+    System.out.println(englishExplanation);
+    
   }
   public static double calculateArea(double length, double width) {
     if (length < 0 || width < 0) {
@@ -26,10 +25,8 @@ public class ReturnValues {
    }
   }
 
+  public static void printArea(double length, double width, double area) {
+    System.out.println("A rectangle with a length of " + length + " and a width of " + width +  " has an area of " + area);
+  }
+
 }
-
-//English "Area equals length * width"
-
-//French "La surface est egale a la longueur * la largeur"
-
-//Spanish "area es igual a largo * ancho"
