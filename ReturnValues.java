66cde2502cc7929bfb5ef53cf44d02dc3c159737
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-07-29 12:22:04 +0900

    updated main

--- ReturnValues.java
+++ ReturnValues.java
@@ -1,5 +1,11 @@
 public class ReturnValues {
   public static void main(String[] args) {
-    
+    double area1 = calculateArea(2.3, 3.6);
+    double area2 = calculateArea(1.6, 2.4);
+    double area3 = calculateArea(2.6, 4.2);
+  }
+  public static double calculateArea(double length, double width) {
+    double area = length * width;
+    return area;
   }
 }
